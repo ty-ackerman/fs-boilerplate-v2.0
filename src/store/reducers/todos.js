b9674dce2bc0f9d52529b6c75d6fc5ac7c355f4b
Ty Ackerman <tyler.raye.ackerman@gmail.com>
2020-04-19 23:38:41 -0400

    BE support added + integrated with Redux

--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -5,35 +5,16 @@ const initialState = {
   todos: []
 }
 
-const storeTodo = (state, action) => {
-  const todos = state.todos.concat({
-    name: action.name,
-    done: false,
-    _id: new Date().getTime()
-  })
-  return updateObject(state, { todos })
-}
-
-const checkTodo = (state, action) => {
-  const todos = state.todos.map(todo => {
-    if (todo._id === action._id) {
-      todo.done = !todo.done
-    }
-    return todo
-  })
-  return updateObject(state, { todos })
-}
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD:
-      return storeTodo(state, action)
+      return updateObject(state, { todos: action.todos })
     case actionTypes.DELETE:
-      return updateObject(state, {
-        todos: state.todos.filter(todo => action._id !== todo._id)
-      })
+      return updateObject(state, { todos: action.todos })
     case actionTypes.CHECK:
-      return checkTodo(state, action)
+      return updateObject(state, { todos: action.todos })
+    case actionTypes.LOAD:
+      return updateObject(state, { todos: action.todos })
     default:
       return state
   }
